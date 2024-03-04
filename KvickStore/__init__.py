21ef44de1a67b6a0aaf6445cbdce3768d2b289e6
Rishabh <panicpark60@gmail.com>
2024-03-04 14:19:30 -0600

    add setup.py

---
+++ KvickStore/__init__.py
@@ -0,0 +1,1 @@
+from .KvickStore import KvickStore
---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="KvickStore",
+    version="0.0.1",
+    author="Rishabh Mediratta",
+    author_email="panicpark60@gmail.com",
+    url="https://github.com/rm206/KvickStore",
+    description="A quick and simple key-value store for Python applications",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/rm206/KvickStore",
+    packages=find_packages(),
+    classifiers=[
+        "Development Status :: 3 - Alpha",  # Indicates package is pre-production
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
