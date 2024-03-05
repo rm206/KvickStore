c941e173fa53fd7570dd165797bf2ea47fcc9d70
Rishabh <panicpark60@gmail.com>
2024-03-04 22:12:08 -0600

    update version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="KvickStore",
-    version="0.0.1",
+    version="0.0.2",
     author="Rishabh Mediratta",
     author_email="panicpark60@gmail.com",
     description="A quick and simple key-value store for Python applications",
